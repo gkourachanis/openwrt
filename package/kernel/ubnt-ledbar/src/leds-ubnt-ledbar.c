e52b98b359934269254c0a18120caff763595bc2
Georgios Kourachanis <geo.kourachanis@gmail.com>
2022-12-20 18:23:44 +0200

    ubnt-ledbar: refresh source code for kernel >= v6.0
    Signed-off-by: Georgios Kourachanis <geo.kourachanis@gmail.com>

--- package/kernel/ubnt-ledbar/src/leds-ubnt-ledbar.c
+++ package/kernel/ubnt-ledbar/src/leds-ubnt-ledbar.c
@@ -218,13 +218,11 @@ static int ubnt_ledbar_probe(struct i2c_client *client,
 	return ubnt_ledbar_apply_state(ledbar);
 }
 
-static int ubnt_ledbar_remove(struct i2c_client *client)
+static void ubnt_ledbar_remove(struct i2c_client *client)
 {
 	struct ubnt_ledbar *ledbar = i2c_get_clientdata(client);
 
 	mutex_destroy(&ledbar->lock);
-
-	return 0;
 }
 
 static const struct i2c_device_id ubnt_ledbar_id[] = {
